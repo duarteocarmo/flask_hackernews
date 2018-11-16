401e96a0006f61aa0651390850decd580b665417
Duarte OC <duarteocarmo@gmail.com>
2018-11-16 13:53:57 +0000

    deleted teste

--- app/teste.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-
-def get_score(time, score, gravity=1.8):
-    datetime_difference = datetime.utcnow() - time
-    hours_passed = (
-        datetime_difference.days * 24 + datetime_difference.seconds / 3600
-    )
-    return (score - 1) / pow((hours_passed + 2), gravity)
