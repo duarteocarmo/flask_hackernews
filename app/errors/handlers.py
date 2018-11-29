7e0dae58a51be7317e941b9d5b198b5233b584d3
Duarte OC <duarteocarmo@gmail.com>
2018-11-29 17:57:54 +0000

    fixed error page display

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -3,12 +3,12 @@ from app import db
 from app.errors import bp
 
 
-@bp.errorhandler(404)
+@bp.app_errorhandler(404)
 def not_found_error(error):
     return render_template("errors/404.html"), 404
 
 
-@bp.errorhandler(500)
+@bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template("errors/500.html"), 500
